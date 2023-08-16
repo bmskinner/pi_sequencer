9a9380a8b6e04cc36d32014a4486e0124024c5c9
Ben Skinner <b.skinner@essex.ac.uk>
2023-08-16 16:02:19 +0100

    Use deque for plotting

--- figTest.py
+++ figTest.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
+import collections
 
 MAX_X_POINTS = 20
 UPDATE_INTERVAL_MS = 150
@@ -10,27 +11,50 @@ def read_y_data():
 	return np.random.randn(1)
 
 # Create the empty figure
-
-fig = plt.figure()  
-sub = fig.add_subplot(111, xlim=(-1, 1), ylim=(-1, 1))
-PLOT, = sub.plot([],[])
-
-# Called on each frame of the animation
 def animate(i):
-	xdata = PLOT.get_xdata()
-	ydata = PLOT.get_ydata()
-	newx = np.append(xdata, datetime.now())
-	newy = np.append(ydata, read_y_data())
+    y = read_y_data()
+    data.append((datetime.now(), y))
+    ax.relim()
+    ax.autoscale_view()
+    ax.set_ylim(-5, 5)
+    ax.set_xlim(data[0][0], data[-1][0])
+    line.set_data(*zip(*data))
 
-	x_start = len(newx) - MAX_X_POINTS if len(newx) > MAX_X_POINTS else 0
+fig, ax = plt.subplots()
+x = datetime.now()
+y = read_y_data()
+data = collections.deque([(x, y)], maxlen=MAX_X_POINTS)
+line, = plt.plot(*zip(*data), c='black')
 
-	sub.clear()
-	sub.plot(newx[x_start:], newy[x_start:])
-	PLOT.set_data(newx, newy )
-	plt.ylabel('Hue')
-	plt.xlabel('Seconds')
-	plt.show()
-	return PLOT,
-
-ani = anim.FuncAnimation(fig, animate, interval=UPDATE_INTERVAL_MS, blit=False)
+ani = anim.FuncAnimation(fig, animate, interval=UPDATE_INTERVAL_MS)
 plt.show()
+
+
+
+# fig = plt.figure()  
+# fig, ax = plt.subplots()
+# sub = fig.add_subplot(111, xlim=(-1, 1), ylim=(-10, 10))
+# PLOT, = sub.plot(x_data,y_data)
+
+# # Called on each frame of the animation
+# def animate(i):
+# 	# xdata = PLOT.get_xdata()
+# 	# ydata = PLOT.get_ydata()
+
+# 	x_data.popleft()
+# 	y_data.popleft()
+
+# 	x_data.append(x_data[-1]+1)
+# 	y_data.append(read_y_data())
+
+# 	print(x_data)
+# 	sub.clear()
+# 	PLOT.set_data(x_data, y_data)
+# 	sub.plot(x_data, y_data)
+
+# 	plt.ylabel('Hue')
+# 	plt.show()
+# 	return PLOT,
+
+# ani = anim.FuncAnimation(fig, animate, interval=UPDATE_INTERVAL_MS, blit=False)
+# plt.show()
